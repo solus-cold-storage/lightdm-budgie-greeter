0bbad7ebfee5dc6333b8116de6f65e4512c93b63
Ikey Doherty <ikey@solus-project.com>
2016-06-30 22:56:36 +0100

    Initial build skeleton inclusion
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/main.c
@@ -0,0 +1,20 @@
+/*
+ * This file is part of lightdm-budgie-greeter.
+ *
+ * Copyright © 2016 Ikey Doherty <ikey@solus-project.com>
+ *
+ * lightdm-budgie-greeter is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ */
+
+#include <stdlib.h>
+#include <gtk/gtk.h>
+
+int main(int argc, char **argv)
+{
+        gtk_init(&argc, &argv);
+
+        return EXIT_SUCCESS;
+}
