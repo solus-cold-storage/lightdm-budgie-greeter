2fdc8de23b9d9c813f35f434a5a6f0303a05767f
Ikey Doherty <ikey@solus-project.com>
2016-07-01 00:22:19 +0100

    Set up the panel area
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/greeter-window.c
+++ src/greeter-window.c
@@ -13,13 +13,14 @@
 #include "util.h"
 
 struct BudgieGreeterWindowPrivate {
-        int x;
+        GtkWidget *top_panel;
 };
 
 /**
  * implemented down the bottom
  */
 static void budgie_greeter_window_init(BudgieGreeterWindow *self);
+static GtkWidget *budgie_greeter_window_create_panel(BudgieGreeterWindow *self);
 
 G_DEFINE_TYPE_WITH_PRIVATE(BudgieGreeterWindow, budgie_greeter_window, GTK_TYPE_WINDOW)
 
@@ -46,6 +47,8 @@ static void budgie_greeter_window_init(BudgieGreeterWindow *self)
         GtkStyleContext *context = NULL;
         GdkVisual *visual = NULL;
         GdkScreen *screen = NULL;
+        GtkWidget *top_panel = NULL;
+        GtkWidget *layout = NULL;
 
         self->priv = budgie_greeter_window_get_instance_private(self);
         g_signal_connect(G_OBJECT(self),
@@ -69,6 +72,15 @@ static void budgie_greeter_window_init(BudgieGreeterWindow *self)
         /* Get some styling in place */
         context = gtk_widget_get_style_context(GTK_WIDGET(self));
         gtk_style_context_add_class(context, "osd");
+
+        /* Sort the layout .. out */
+        layout = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+        gtk_container_add(GTK_CONTAINER(self), layout);
+
+        /* Create the fake top panel */
+        top_panel = budgie_greeter_window_create_panel(self);
+        self->priv->top_panel = top_panel;
+        gtk_box_pack_start(GTK_BOX(layout), top_panel, FALSE, FALSE, 0);
 }
 
 /**
@@ -87,6 +99,44 @@ static void budgie_greeter_window_class_init(BudgieGreeterWindowClass *klazz)
 {
         GObjectClass *obj_class = G_OBJECT_CLASS(klazz);
         obj_class->dispose = budgie_greeter_window_dispose;
+}
+
+/**
+ * Create a panel area to emulate BudgiePanel
+ */
+static GtkWidget *budgie_greeter_window_create_panel(BudgieGreeterWindow *self)
+{
+        GtkWidget *ebox = NULL;
+        GtkWidget *layout = NULL;
+        GtkWidget *shadow = NULL;
+        GtkWidget *panel_area = NULL;
+        GtkStyleContext *context = NULL;
+
+        ebox = gtk_event_box_new();
+        context = gtk_widget_get_style_context(ebox);
+        gtk_style_context_add_class(context, "budgie-container");
+
+        /* Fix layout */
+        layout = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+        gtk_container_add(GTK_CONTAINER(ebox), layout);
+
+        /* Create the ""panel"" */
+        panel_area = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+        context = gtk_widget_get_style_context(panel_area);
+        gtk_style_context_add_class(context, "budgie-panel");
+
+        gtk_box_pack_start(GTK_BOX(layout), panel_area, FALSE, FALSE, 0);
+        gtk_widget_set_size_request(panel_area, -1, 35);
+
+        /* Create the "shadow" */
+        shadow = gtk_event_box_new();
+        gtk_widget_set_size_request(shadow, -1, 5);
+        context = gtk_widget_get_style_context(shadow);
+        gtk_style_context_add_class(context, "shadow-block");
+
+        gtk_box_pack_start(GTK_BOX(layout), shadow, FALSE, FALSE, 0);
+
+        return ebox;
 }
 
 /*
